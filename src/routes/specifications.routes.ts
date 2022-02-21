402f72f9e196ff9994939fcb223bace55e1ccda3
Gabriel Andrade <gandradevini@gmail.com>
2022-02-21 07:34:17 -0300

    Adding middleware to ensure authentication

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ensureAuthentication } from "../middleware/ensureAuthentication";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications/ListSpecificationsController";
 
@@ -8,6 +9,7 @@ const specificationRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
+specificationRoutes.use(ensureAuthentication);
 specificationRoutes.post("/", createSpecificationController.handle);
 
 specificationRoutes.get("/", listSpecificationsController.handle);
