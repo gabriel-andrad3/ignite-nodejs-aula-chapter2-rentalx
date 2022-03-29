2ddeafcb2026ee70dd94014ce7667e8ef67f374a
Gabriel Andrade <gandradevini@gmail.com>
2022-03-29 00:10:41 -0300

    refactoring folders segregating responsibilities

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ListSpecificationsController } from "@modules/cars/useCases/listSpecifications/ListSpecificationsController";
+
 import { ensureAuthentication } from "../middleware/ensureAuthentication";
-import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
-import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications/ListSpecificationsController";
 
 const specificationRoutes = Router();
 
